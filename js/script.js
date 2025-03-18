819fcd16679922a5d1fc29a2cc7b3c342bbe8637
Ignacio Talvi <ignaciotalvi@Ignacios-MacBook-Pro.local>
2025-03-18 09:54:43 +0100

    v3

--- js/script.js
+++ js/script.js
@@ -18,20 +18,20 @@ document.querySelector("form").addEventListener("submit", function (event) {
   const fname = event.target.fname.value;
   const lname = event.target.lname.value;
   const email = event.target.email.value;
-  const phone = event.target.phone.value;
-  const mensaje = event.target.mensaje.value;
+  const phone = event.target.tel.value;
+  //   const mensaje = event.target.mensaje.value;
 
   let msj = "";
 
   if (fname.length < 3 || fname.length > 30) {
     //err
-    console.log("Nombre fuera de tamaño: 3-30");
+    alert("Nombre fuera de tamaño: 3-30");
     msj += "Nombre fuera de tamaño: 3-30\n";
   }
 
   // 3 < lname < 30 required
   if (lname.length < 3 || lname.length > 30) {
-    console.log("Apellido fuera de tamaño: 3-30");
+    alert("Apellido fuera de tamaño: 3-30");
     msj += "Apellido fuera de tamaño: 3-30\n";
   }
 
@@ -39,16 +39,27 @@ document.querySelector("form").addEventListener("submit", function (event) {
     (!email.endsWith(".com") && !email.endsWith(".es")) ||
     !email.includes("@")
   ) {
-    console.log("Error validación: " + email);
+    alert("Error validación: " + email);
     msj += "Error validación: " + email + "\n";
   }
   if (phone.length === !9) {
-    console.log("Telefono incorrecto");
+    alert("Telefono incorrecto");
     msj += "Telefono incorrecto";
   }
   if (mensaje.length < 30) {
-    console.log("Telefono incorrecto");
+    alert("Telefono incorrecto");
     msj += "Telefono incorrecto";
+  }
+  if (msj.length != 0) {
+    alert(msj); //imprime mensaje final de error
+    //document.body.innerHTML += msj;
+    let p = document.createElement("pre");
+    let mensaje = document.createTextNode(msj);
+    p.style.color = "#DD1C1A";
+    p.style.fontSize = "16px";
+    p.appendChild(mensaje);
+
+    document.getElementById("contact").appendChild(p); // dibuja resultado
   } else {
     //Enviar formulario
     document.querySelector("form").submit();
